fb5eb4b13b24150c0141533342ef1ee43e5a79c6
sorushbin <mimisaif@gmail.com>
2024-08-01 22:47:12 +0330

    create accounts app
    - create accounts app
    -create user and profile model
    -customize user
    -admin in accounts app

---
+++ core/accounts/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.validators import validate_iranian_cellphone_number
+
+class UserType(models.IntegerChoices):
+    customer = 1, _("customer")
+    admin = 2, _("admin")
+    superuser = 3, _("superuser")
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        create and save a user with the given email and password and extra fields.
+        """
+        if not email:
+            raise ValueError(("the Email must be set."))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        create and save a Superuser with the given email and password and extra fields.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_verified", True)
+        extra_fields.setdefault("type", UserType.superuser.value)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    this is a model for accounts
+    """
+
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    type = models.IntegerField(choices=UserType.choices, default=UserType.customer.value)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=11, validators=[validate_iranian_cellphone_number])
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created and instance.type == UserType.customer.value:
+        Profile.objects.create(user=instance, id=instance.id)
