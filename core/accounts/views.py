b6bdf35823bc91ebf965815d5506d356046cdae7
sorushbin <mimisaif@gmail.com>
2024-08-02 13:22:47 +0330

    login and logout and messages error
    - show messages with toastify
    -login view and its template
    -logout view and related settings

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from .forms import AuthenticationForm
 
-# Create your views here.
+class LoginView(auth_views.LoginView):
+    form_class = AuthenticationForm
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = True
+
+class LogoutView(auth_views.LogoutView):
+    pass
--- core/core/settings.py
+++ core/core/settings.py
@@ -141,7 +141,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp4dev') 
 EMAIL_PORT = config('EMAIL_PORT', cast=int, default=25)             
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=False)          
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)               
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False) 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')  
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')   
 
@@ -159,5 +159,9 @@ if SHOW_DEBUGGER_TOOLBAR:
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
-
+# customize user model
 AUTH_USER_MODEL = 'accounts.User'
+
+# redirect to home
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
